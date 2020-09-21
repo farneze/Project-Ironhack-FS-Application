c6fe26c447532bc67107ea08741281826fee305c
Arthur Farneze <arthurfarneze@gmail.com>
2020-09-21 13:17:23 -0300

    2nd Initial Commit

--- app.js
+++ app.js
@@ -1,120 +1,63 @@
 require("dotenv").config();
-
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
+const path = require("path");
 const express = require("express");
+const createError = require("http-errors");
+const logger = require("morgan");
 const favicon = require("serve-favicon");
+
+const cookieParser = require("cookie-parser");
 const hbs = require("hbs");
 const mongoose = require("mongoose");
-const logger = require("morgan");
-const path = require("path");
 
-const session = require("express-session");
-const MongoStore = require("connect-mongo")(session);
+// Set up the database
+require("./configs/db.config");
 
-const bcrypt = require("bcrypt");
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
+// Routers
+const indexRouter = require("./routes/index.routes");
 
-const User = require("./models/user.js");
+const app = express();
 
-const flash = require("connect-flash");
+// Configurar sessoes
+// Aqui estamos usando um conceito chamado currying: uma funcao que retorna outra funcao. Podemos invocar a funcao de dentro sem atribui-la a uma variavel antes
+require("./configs/session.config")(app);
 
-mongoose
-  .connect("mongodb://localhost/lab-passport", { useNewUrlParser: true })
-  .then((x) => {
-    console.log(
-      `Connected to Mongo! Database name: "${x.connections[0].name}"`
-    );
-  })
-  .catch((err) => {
-    console.error("Error connecting to mongo", err);
-  });
+// Configurar passport
+require("./configs/passport.config")(app);
+
+// Express View engine setup
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+hbs.registerPartials(__dirname + "/views/partials");
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 const app_name = require("./package.json").name;
 const debug = require("debug")(
   `${app_name}:${path.basename(__filename).split(".")[0]}`
 );
 
-const app = express();
+// Routes middleware
+app.use("/", indexRouter);
 
-//connect-flash
-app.use(flash());
+// Catch missing routes and forward to error handler
+app.use((req, res, next) => next(createError(404)));
 
-// Middleware Setup
-app.use(logger("dev"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+// Catch all error handler
+app.use((error, req, res) => {
+  // Set error information, with stack only available in development
+  res.locals.message = error.message;
+  res.locals.error = req.app.get("env") === "development" ? error : {};
 
-// Express View engine setup
-
-app.use(
-  require("node-sass-middleware")({
-    src: path.join(__dirname, "public"),
-    dest: path.join(__dirname, "public"),
-    sourceMap: true,
-  })
-);
-
-app.use(
-  session({
-    secret: "our-passport-local-strategy-app",
-    store: new MongoStore({ mongooseConnection: mongoose.connection }),
-    resave: true,
-    saveUninitialized: true,
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.serializeUser((user, cb) => {
-  cb(null, user._id);
+  // render the error page
+  res.status(error.status || 500);
+  res.render("error");
 });
-passport.deserializeUser((id, cb) => {
-  User.findById(id)
-    .then((user) => cb(null, user))
-    .catch((err) => cb(err));
-});
-
-passport.use(
-  new LocalStrategy(
-    {
-      usernameField: "username", // by default
-      passwordField: "password", // by default
-    },
-    (username, password, done) => {
-      User.findOne({ username })
-        .then((user) => {
-          if (!user) {
-            return done(null, false, { message: "Incorrect username" });
-          }
-
-          if (!bcrypt.compareSync(password, user.password)) {
-            return done(null, false, { message: "Incorrect password" });
-          }
-
-          done(null, user);
-        })
-        .catch((err) => done(err));
-    }
-  )
-);
-
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "hbs");
-app.use(express.static(path.join(__dirname, "public")));
-app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
-
-// default value for title local
-app.locals.title = "Welcome to our NEW page";
-
-// const router = require("./routes/auth-routes");
-// app.use("/", router);
-const index = require("./routes/index");
-app.use("/", index);
-const router = require("./routes/auth-routes");
-app.use("/", router);
 
 module.exports = app;
